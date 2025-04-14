9c120a7f0ba580826ca4ef16405ba9a486470076
keshav-const <keshavladdha005@gmail.com>
2025-04-14 15:50:19 +0530

    components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/navigation/Navigation.jsx';
-import Product from './customer/components/Product/Product.jsx';
+import Product from './customer/components/Product//Product/Product.jsx';
 import HomePage from './customer/pages/HomePage/HomePage';
 
 function App() {
